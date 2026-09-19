9ea436efa6a0fb110a6abc28677f64c5b28b6fb1
DrZhaology <3971624496@qq.com>
2026-09-19 08:43:33 +0800

    chore: build-exe.ps1 版本号 0.4.0 -> 0.4.1

--- build-exe.ps1
+++ build-exe.ps1
@@ -13,7 +13,7 @@
 $ErrorActionPreference = 'Stop'
 $root = Split-Path -Parent $MyInvocation.MyCommand.Path
 $destDir = 'E:\Develop'
-$ver = '0.4.0'
+$ver = '0.4.1'
 $appDir = Join-Path $root 'dist\win-unpacked'
 $zipName = "BlockCanvas-$ver-win64.zip"
 $zipPath = Join-Path $root "dist\$zipName"
